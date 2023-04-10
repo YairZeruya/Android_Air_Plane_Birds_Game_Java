8b5d149fa8f0c9c879aa7a0069d9166190ad9346
YairZeruya <Zeruya95@gmail.com>
2023-04-10 17:04:38 +0300

    Final Project

---
+++ app/src/main/java/com/example/hw1/Logic/GameManager.java
@@ -0,0 +1,207 @@
+package com.example.hw1.Logic;
+
+import static com.example.hw1.MainActivity.OBSTACLE_COLUMNS;
+import static com.example.hw1.MainActivity.OBSTACLE_ROWS;
+
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+
+import com.example.hw1.R;
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameManager {
+
+    //Hearts
+    private int life;
+    private int startLife;
+    private boolean[] isHeartVisible;
+
+    //Air Plane
+    private int airPlaneLocation;
+    private boolean[] isAirPlaneVisible;
+
+    //Obstacles
+    private boolean[][] isObstacleVisible;
+    private int obstaclesInGame;
+    private Random obstacleStartPlace;
+    private ArrayList<Integer> obstaclesIndexArray;//10 = [1][0] 22 = [2][2]
+
+
+    public GameManager(int life, ShapeableImageView[] airPlaneUI, ShapeableImageView[][] obstaclesUi) {
+        initHearts(life);
+        initAirplane(airPlaneUI);
+        initObstacles(obstaclesUi);
+    }
+
+    //*********************** Hearts Methods *************************
+
+    public void initHearts(int life) {
+        this.life = life;
+        this.startLife = 3;
+        isHeartVisible = new boolean[life];
+        for (int i = 0; i < life; i++) {
+            isHeartVisible[i] = true;
+        }
+    }
+
+    public void updateHeartUI(ShapeableImageView[] heartsUI) {
+        for (int i = 0; i < startLife; i++) {
+            ShapeableImageView sim = heartsUI[i];
+            if (!isHeartVisible[i])
+                sim.setVisibility(View.INVISIBLE);
+            else {
+                sim.setVisibility(View.VISIBLE);
+                sim.setImageResource(R.drawable.ic_icon_heart);
+            }
+        }
+    }
+
+    public boolean isLose() {
+        return life == 0;
+    }
+
+    public void loseLife(ShapeableImageView[] heartsUI) {
+        isHeartVisible[life - 1] = false;
+        life--;
+        updateHeartUI(heartsUI);
+    }
+
+
+    //*********************** Air Plane Methods ************************
+
+
+    public void initAirplane(ShapeableImageView[] AirPlaneUI) {
+        airPlaneLocation = 1;
+        isAirPlaneVisible = new boolean[OBSTACLE_COLUMNS];
+        isAirPlaneVisible[OBSTACLE_COLUMNS / 2] = true;
+        updateAirPlaneUI(AirPlaneUI);
+    }
+
+    public boolean moveAirPlaneLeft(ShapeableImageView[] AirPlaneUI) {
+        if (airPlaneLocation == 0) {
+            return false;
+        }
+        isAirPlaneVisible[airPlaneLocation] = false;
+        airPlaneLocation -= 1;
+        isAirPlaneVisible[airPlaneLocation] = true;
+        updateAirPlaneUI(AirPlaneUI);
+        return true;
+    }
+
+    public boolean moveAirPlaneRight(ShapeableImageView[] AirPlaneUI) {
+        if (airPlaneLocation == 2) {
+            return false;
+        }
+        isAirPlaneVisible[airPlaneLocation] = false;
+        airPlaneLocation += 1;
+        isAirPlaneVisible[airPlaneLocation] = true;
+        updateAirPlaneUI(AirPlaneUI);
+        return true;
+    }
+
+    public void updateAirPlaneUI(ShapeableImageView[] AirPlaneUI) {
+        for (int i = 0; i < OBSTACLE_COLUMNS; i++) {
+            ShapeableImageView sim = AirPlaneUI[i];
+            if (!isAirPlaneVisible[i])
+                sim.setVisibility(View.INVISIBLE);
+            else {
+                sim.setVisibility(View.VISIBLE);
+                sim.setImageResource(R.drawable.baseline_local_airport_24);
+            }
+        }
+    }
+
+    public int getAirPlaneLocation() {
+        return airPlaneLocation;
+    }
+
+    public boolean checkAirPlaneFlag(int i) {
+        return isAirPlaneVisible[i];
+    }
+
+    public void setAirPlaneVisibility(int i, boolean value) {
+        this.isAirPlaneVisible[i] = value;
+    }
+
+
+    //********************* Obstacle Methods **********************************
+
+
+    public void initObstacles(ShapeableImageView[][] obstaclesUI) {
+        isObstacleVisible = new boolean[OBSTACLE_ROWS][OBSTACLE_COLUMNS];
+        obstacleStartPlace = new Random();
+        obstaclesIndexArray = new ArrayList<>();
+        updateObstacleUI(obstaclesUI);
+        obstaclesInGame = 0;
+    }
+
+    public int getObstaclesInGame() {
+        return obstaclesInGame;
+    }
+
+    public void setObstaclesInGame(int obstaclesInGame) {
+        this.obstaclesInGame = obstaclesInGame;
+    }
+
+    public void updateObstacleUI(ShapeableImageView[][] obstaclesUI) {
+        for (int i = 0; i < OBSTACLE_ROWS; i++) {
+            for (int j = 0; j < OBSTACLE_COLUMNS; j++) {
+                ShapeableImageView sim = obstaclesUI[i][j];
+                if (!isObstacleVisible[i][j]) {
+                    sim.setVisibility(View.INVISIBLE);
+                } else {
+                    sim.setVisibility(View.VISIBLE);
+                    sim.setImageResource(R.drawable.bird_svgrepo_com);
+                }
+            }
+        }
+    }
+
+    public void updateObstaclePlace(int i, ShapeableImageView[][] obstaclesUI) {
+
+        int rowIndex = obstaclesIndexArray.get(i) / 10;
+        int columnIndex = obstaclesIndexArray.get(i) % 10;
+        if (isObstacleVisible[rowIndex][columnIndex]) {
+            this.isObstacleVisible[rowIndex][columnIndex] = false;
+            this.isObstacleVisible[rowIndex + 1][columnIndex] = true;
+            obstaclesIndexArray.set(i, obstaclesIndexArray.get(i) + 10);//next row
+            updateObstacleUI(obstaclesUI);
+        }
+    }
+
+    public ArrayList<Integer> getObstaclesIndexArray() {
+        return obstaclesIndexArray;
+    }
+
+    public void setObstacleVisibility(int i, int j, boolean value) {
+        this.isObstacleVisible[i][j] = value;
+    }
+
+    public void createObstacle(ShapeableImageView[][] obstaclesUI) {
+        int randColumn = obstacleStartPlace.nextInt(3);
+        if (!isObstacleVisible[0][randColumn]) {
+            isObstacleVisible[0][randColumn] = true;
+            obstaclesIndexArray.add(randColumn);
+        }
+        obstaclesInGame++;
+        updateObstacleUI(obstaclesUI);
+    }
+
+    public void vibrate(Vibrator v, int ms) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(ms, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v.vibrate(ms);
+        }
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/example/hw1/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.hw1.Logic.GameManager;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int OBSTACLE_COLUMNS = 3;
+    public static final int OBSTACLE_ROWS = 6;
+    private ExtendedFloatingActionButton main_left_button;
+    private ExtendedFloatingActionButton main_right_button;
+    private ShapeableImageView[] main_IMG_hearts;
+    private ShapeableImageView[][] main_IMG_obstacles;
+    private ShapeableImageView[] main_IMG_Air_Planes;
+    private GameManager gameManager;
+    private final Timer createObstacleTimer = new Timer();
+    private final Timer moveObstacleTimer = new Timer();
+    private final int MOVE_FREQUENCY = 1000;
+    private final int CREATE_FREQUENCY = 2000;
+    private final int DELAY = 500;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViews();
+        gameManager = new GameManager(main_IMG_hearts.length, main_IMG_Air_Planes, main_IMG_obstacles);
+        refreshUI();
+    }
+
+    private void gameTimer() {
+        createObstacleTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(() -> gameManager.createObstacle(main_IMG_obstacles));
+            }
+        }, 0, CREATE_FREQUENCY);
+
+
+        moveObstacleTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(() -> moveObstacle());
+            }
+        }, DELAY, MOVE_FREQUENCY);
+
+    }
+
+    private void refreshUI() {
+        gameTimer();
+        main_left_button.setOnClickListener(v -> gameManager.moveAirPlaneLeft(main_IMG_Air_Planes));
+        main_right_button.setOnClickListener(v -> gameManager.moveAirPlaneRight(main_IMG_Air_Planes));
+    }
+
+
+    private void findViews() {
+        main_left_button = findViewById(R.id.left_FAB_button);
+        main_right_button = findViewById(R.id.right_FAB_button);
+        main_IMG_hearts = new ShapeableImageView[]{
+                findViewById(R.id.main_IMG_heart1),
+                findViewById(R.id.main_IMG_heart2),
+                findViewById(R.id.main_IMG_heart3)};
+        main_IMG_obstacles = new ShapeableImageView[][]{
+                {findViewById(R.id.main_matrix_00),
+                        findViewById(R.id.main_matrix_01),
+                        findViewById(R.id.main_matrix_02)},
+                {findViewById(R.id.main_matrix_10),
+                        findViewById(R.id.main_matrix_11),
+                        findViewById(R.id.main_matrix_12)},
+                {findViewById(R.id.main_matrix_20),
+                        findViewById(R.id.main_matrix_21),
+                        findViewById(R.id.main_matrix_22)},
+                {findViewById(R.id.main_matrix_30),
+                        findViewById(R.id.main_matrix_31),
+                        findViewById(R.id.main_matrix_32)},
+                {findViewById(R.id.main_matrix_40),
+                        findViewById(R.id.main_matrix_41),
+                        findViewById(R.id.main_matrix_42)},
+                {findViewById(R.id.main_matrix_50),
+                        findViewById(R.id.main_matrix_51),
+                        findViewById(R.id.main_matrix_52)}};
+        main_IMG_Air_Planes = new ShapeableImageView[]{
+                findViewById(R.id.main_matrix_60),
+                findViewById(R.id.main_matrix_61),
+                findViewById(R.id.main_matrix_62)};
+    }
+
+    private void moveObstacle() {
+        for (int i = 0; i < gameManager.getObstaclesInGame(); i++) {
+            if (gameManager.getObstaclesIndexArray().get(i) < (OBSTACLE_ROWS - 1) * 10) {//[5][j] is max row of obstacle
+                gameManager.updateObstaclePlace(i, main_IMG_obstacles);
+            } else if (gameManager.getObstaclesIndexArray().get(i) >= (OBSTACLE_ROWS - 1) * 10) { // going to the last row
+                gameManager.setObstacleVisibility
+                        (gameManager.getObstaclesIndexArray().get(i) / 10
+                                , gameManager.getObstaclesIndexArray().get(i) % 10
+                                , false);
+                moveObstacleToLastRow(i);
+                gameManager.getObstaclesIndexArray().remove(i);
+                gameManager.updateObstaclePlace(i, main_IMG_obstacles);
+                gameManager.updateObstacleUI(main_IMG_obstacles);
+                gameManager.setObstaclesInGame(gameManager.getObstaclesInGame() - 1);
+                new Handler().postDelayed(() -> gameManager.updateAirPlaneUI(main_IMG_Air_Planes), DELAY);
+            }
+        }
+    }
+
+    private void moveObstacleToLastRow(int i) {
+        ShapeableImageView sim = main_IMG_Air_Planes[gameManager.getObstaclesIndexArray().get(i) % 10];
+        sim.setImageResource(R.drawable.bird_svgrepo_com);
+        sim.setVisibility(View.VISIBLE);
+        if (gameManager.checkAirPlaneFlag(gameManager.getObstaclesIndexArray().get(i) % 10)) {
+            crash(sim);
+        }
+    }
+
+    private void crash(ShapeableImageView sim) {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        gameManager.loseLife(main_IMG_hearts);
+        sim.setImageResource(R.drawable.collision_svgrepo_com);
+        sim.setVisibility(View.VISIBLE);
+        if (gameManager.isLose()) {
+            Toast.makeText(this, "Game Over!", Toast.LENGTH_SHORT).show();
+            gameManager.vibrate(v, 1500);
+        } else {
+            Toast.makeText(this, "crash!", Toast.LENGTH_SHORT).show();
+            gameManager.vibrate(v, 500);
+            gameManager.setAirPlaneVisibility(gameManager.getAirPlaneLocation(), true);
+        }
+    }
+
+}
+
